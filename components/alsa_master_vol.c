176fe13cafe1643b7d65171aff99873957bcf5ba
Saúl Valdelvira <saul@saulv.es>
2025-11-07 15:31:11 +0100

    Make the alsa master block more robust
    Sometimes amixer get returns a "[0.00dB]" in between.

--- components/alsa_master_vol.c
+++ components/alsa_master_vol.c
@@ -7,18 +7,14 @@
 const char *
 alsa_master_vol(const char *unused)
 {
-        char *cmd = "amixer get Master | tail -n 1 | awk -F'[][]' '{ \
-                     printf $2;\
-                     if ($4 == \"on\") \
-                             print 1; \
-                     else print 0; \
-        }'";
+        char *cmd = "amixer get Master | tail -n 1 | sed 's/.*\\[\\(.*\\)%\\].*\\(on\\|off\\).*/\\1%\\2/'";
 
 	FILE *fp = popen(cmd, "r");
         int vol;
-        int is_on;
-        fscanf(fp, "%d%%%d", &vol, &is_on);
+        char on_off[4] = {0};
+        fscanf(fp, "%d%%%3s", &vol, on_off);
         pclose(fp);
+        int is_on = strcmp(on_off, "on") == 0;
 
         char *icon = is_on ? "\U000f057e"  : "\U000f0581";
         return bprintf("%s %d%%", icon, vol);
