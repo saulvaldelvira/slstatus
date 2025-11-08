5255e49ea9538c6140ccf108565c7388c8002707
Saúl Valdelvira <saul@saulv.es>
2025-11-08 14:21:15 +0100

    Fix callendar cmd when there are no apointments

--- components/calcurse_next.sh
+++ components/calcurse_next.sh
@@ -4,6 +4,7 @@ next_apt=$(calcurse -Q --from today -r2 --filter-type apt --format-apt "%m;%s;%e
 
 curr=$(date +%s)
 while IFS=';' read -r name start end ; do
+    [ -z $start ] && continue
     if [ "$start" -gt "$curr" ]; then
         s=$(date --date="@$start" "+%H:%M")
         echo "$name ($s)"
